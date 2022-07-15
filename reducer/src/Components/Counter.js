82bffad23e269ec2d9c30397151dc7757af2d935
jrii1512 <jriimala@gmail.com>
2022-07-15 09:20:28 +0300

    css tunings for reducer

--- reducer/src/Components/Counter.js
+++ reducer/src/Components/Counter.js
@@ -1,4 +1,5 @@
 import {useReducer} from "react"
+import "../index.css"
 
 //Target to learn how reducer works. Counter app increase numbers based on user clicks.
 //https://dmitripavlutin.com/react-usereducer/
@@ -11,9 +12,14 @@ export default function Counter(){
     const [state, dispatch] = useReducer(reducer, initialState)
     
 return(
-    <div className = "Counter">
-        <h3>{state.current}</h3>
-        <button onClick = {() => dispatch({type: "Add"})}>Add</button>
+    <div className="Counter--main">
+        <h3 className="Counter--title">Reducer</h3>
+        <h3 className = "Counter--value">{state.current}</h3>
+        <div className = "Counter--buttons">
+            <button onClick = {() => dispatch({type: "Add"})}>Add</button>
+            <button onClick = {() => dispatch({type: "Reduce"})}>Reduce</button>
+
+        </div>
     </div>
 )
 }
@@ -22,6 +28,10 @@ const reducer = (state, action) => {
 switch(action.type){
     case "Add":
         return {...state, current: state.current +1 }
+    case "Reduce":
+        return {...state, current:state.current -1 }  
+    default:
+        throw new Error()      
 }
 }
 
